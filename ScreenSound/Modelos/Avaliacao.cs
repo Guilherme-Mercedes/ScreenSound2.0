cae4b9711738643b10e4d1f860cf6498bdf09689
Guilherme Souza Mercedes <guilhermesouzamercedes@gmail.com>
2025-06-18 14:06:03 -0300

    Alterar funcao de registrar album

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -4,9 +4,28 @@ internal class Avaliacao
 {
     public Avaliacao(int nota) 
     {
-        Nota = nota;
+
+        // Validação da nota para garantir que esteja entre 0 e 10, futuramente posso ajustar com try catch 
+        if (nota < 0)
+        {
+            Nota = 0;
+        }
+        else if (nota > 10)
+        {
+            Nota = 10;
+        }
+        else
+        {
+            Nota = nota;
+        }
     }
 
     public int Nota { get; }
+
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
 
 }
