9a4b7aee8c5f49b3fd39c10fc1000c6da074e415
Guilherme Souza Mercedes <guilhermesouzamercedes@gmail.com>
2025-07-07 17:47:48 -0300

    Finalizar o projeto (API DESATIVADA)

--- ScreenSound/Modelos/ResumoIA.cs
+++ ScreenSound/Modelos/ResumoIA.cs
@@ -8,8 +8,9 @@ namespace ScreenSound.Modelos
         {
             //Configuração do api key do Gemini AI
             //Utilizei o NGet GenerativeAI para facilitar a integração com o Gemini AI
-            //Na aula da Alura eles utilizar o OpenAI, mas como é uma api paga o gemini é uma alternativa gratuita para testes 
-            var googleAI = new GoogleAi("AIzaSyCGnESHIYaDnPYy4wTVnRC5pYK8fmhNZJ4");
+            //Na aula da Alura eles utilizar o OpenAI, mas como é uma api paga o gemini é uma alternativa gratuita para testes
+            //essa api nao é mais valida, você precisa criar uma nova no site do Google
+            var googleAI = new GoogleAi("API AQUI");
             var googleModel = googleAI.CreateGenerativeModel("models/gemini-2.0-flash");
             var prompt = $"faca o resumo bem formatado e com poucas linhas da banda {nomeBanda}, e seja informal";
             var resposta = await googleModel.GenerateContentAsync(prompt);
